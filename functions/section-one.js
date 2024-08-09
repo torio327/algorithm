335c15401de11483dd82713b5154863b39539ca3
torio <raikan327@icloud.com>
2024-08-09 14:27:47 +0900

    Sort one

--- functions/section-one.js
+++ functions/section-one.js
@@ -2,17 +2,22 @@
 
 const list=[5,9,3,1,2,8,4,7,6]
  const sorting=(list)=>{
-    for (let i=0;i<list.length;i++){
-        for(let j=i;j<list.length+1;j++){
-        if(list[j]>list[j+1]){
-            list[j]=list[j+1]
+    let a=list
+    for (let i=0;i<a.length;i++){
+        for(let j=0;j<a.length+1;j++){
+        if(a[j]>a[j+1]){
+           kari=a[j]
+            c=a[j+1]
+            a[j]=c
+            a[j+1]=kari
         }
+
         }
     }
-    return list
+    return a
 }
 
-
+console.log('hello')
 
 module.exports={
     sorting
